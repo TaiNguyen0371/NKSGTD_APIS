fcdaeff051cff706bec2f68695040bb06da1f14d
TaiNguyen0371 <nguyentai0371@gmail.com>
2024-08-12 15:30:13 +0700

    fix cors

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://nhakhoasaigontamduc.com");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header(
     "Access-Control-Allow-Headers",
