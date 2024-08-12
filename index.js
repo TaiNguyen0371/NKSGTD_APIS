acbc17b68ec1136d9380a3d41e2f520c85ff3da5
TaiNguyen0371 <nguyentai0371@gmail.com>
2024-08-12 14:53:46 +0700

    .

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://nhakhoasaigontamduc.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use("/apis", require("./src/routes"));
 
 const server = http.createServer(app);
