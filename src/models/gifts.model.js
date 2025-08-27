711d7c05f314b1661575257caef5fc355ce38c75
TaiNguyen0371 <nguyentai0371@gmail.com>
2025-08-27 11:04:17 +0700

    .

--- src/models/gifts.model.js
+++ src/models/gifts.model.js
@@ -1,16 +1,15 @@
 const mongoose = require("mongoose");
 
 const Gift = new mongoose.Schema(
-    {
-        _id: { type: mongoose.Schema.Types.ObjectId, required: true, auto: true },
-        name: { type: String, required: true },
-        desc: { type: String, required: true },
-        price: { type: Number, required: true },
-        image: { type: String, required: true },
-    },
-    {
-        timestamps: true,
-    }
+  {
+    _id: { type: mongoose.Schema.Types.ObjectId, required: true, auto: true },
+    name: { type: String, required: true },
+    ratio: { type: Number, required: true },
+    degree: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.models.Gift || mongoose.model("Gift", Gift, "gifts");
