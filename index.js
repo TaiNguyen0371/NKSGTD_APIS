e8eaf590121d2c6ed9b03ebec428ba41f936d48e
TaiNguyen0371 <141476794+TaiNguyen0371@users.noreply.github.com>
2026-06-27 07:09:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const http = require("http");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const db = require("./src/db/config");
 
-const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 const app = express();
@@ -21,25 +19,14 @@ app.use((req, res, next) => {
   );
   next();
 });
+
 app.use(logger("dev"));
 app.use(cookieParser());
 app.use(express.json());
 
-
-
 app.use("/apis", require("./src/routes"));
 
-const start = async () => {
-    try {
-        await db.connect();
+// Connect DB khi function được khởi tạo
+db.connect();
 
-        server.listen(PORT, () => {
-            console.log(`Server running at ${PORT}`);
-        });
-
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-start();
+module.exports = app;
